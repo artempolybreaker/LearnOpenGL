26573f63e1092dd7f39bae923a90797e60dd23b9
meralium <polybreaker@gmail.com>
2024-01-18 01:39:34 +0100

    cmake: added function which creates a symlink to resource folder

--- Functions.cmake
+++ Functions.cmake
@@ -6,3 +6,21 @@ function(CopyResource Name)
       install(DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/resource" DESTINATION example/${Name})
   endif()
 endfunction()
+
+function(CreateResourceSymlink Name)
+  cmake_parse_arguments(LINK "" "" "COMMON" ${ARGN})
+
+  if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/resource)
+      # Define the target directory for the symlink
+      set(target_symlink_dir "$<TARGET_FILE_DIR:${Name}>/resource")
+
+      # Add a custom command to create the symlink after the target is built
+      add_custom_command(TARGET ${Name} POST_BUILD
+                         COMMAND ${CMAKE_COMMAND} -E create_symlink 
+                                 "${CMAKE_CURRENT_SOURCE_DIR}/resource" 
+                                 "${target_symlink_dir}")
+
+      # Optionally, install the symlink if needed (uncomment if required)
+      # install(DIRECTORY "${target_symlink_dir}" DESTINATION example/${Name})
+  endif()
+endfunction()
